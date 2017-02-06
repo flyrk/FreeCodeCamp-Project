4ac26224d0312ddf580ebfd630985b0953969241
flyrk <simon_xiameng@126.com>
2017-02-06 17:08:53 +0800

    settle down the click problem

--- pj2-build_a_tic_tac_toe_game/scripts/action.js
+++ pj2-build_a_tic_tac_toe_game/scripts/action.js
@@ -1,4 +1,26 @@
-var singlePlayer = document.getElementsByClassName('game-singleP');
-singlePlayer.click(function() {
-    document.getElementsByClassName('startView').style.display = "none";
+var start = false;
+var singlePlayer = false;
+var twoPlayer = false;
+
+function init() {
+
+}
+
+window.addEventListener("load", function() {
+    init();
+    var player = document.getElementsByClassName("player");
+    player[0].addEventListener("click", singlePlayerView);
+    player[1].addEventListener("click", twoPlayerView);
+
 });
+
+function changeStartView() {
+    // console.log("CheckBugs");
+    if(!start) {
+        document.getElementsByClassName('startView')[0].style.display = "none";
+        start = true;
+    } else {
+        document.getElementsByClassName('startView')[0].style.display = "block";
+        start = false;
+    }
+}
---
+++ pj2-build_a_tic_tac_toe_game/scripts/view.js
@@ -0,0 +1,11 @@
+function singlePlayerView() {
+    changeStartView();
+    singlePlayer = true;
+    showGrids();
+}
+
+function twoPlayerView() {
+    changeStartView();
+    twoPlayer = true;
+    showGrids();
+}
