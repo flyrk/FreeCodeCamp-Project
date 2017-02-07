ca25f515e14d5ef50d136517609ac7dc293c1b77
flyrk <simon_xiameng@126.com>
2017-02-07 12:15:35 +0800

    2017.2.7 working at morning finished

--- pj2-build_a_tic_tac_toe_game/scripts/action.js
+++ pj2-build_a_tic_tac_toe_game/scripts/action.js
@@ -1,9 +1,13 @@
 var start = false;
 var singlePlayer = false;
 var twoPlayer = false;
+var backBoad = document.getElementsByClassName("game-background");
+var turn = null;
 
 function init() {
-
+    start = false;
+    singlePlayer = false;
+    twoPlayer = false;
 }
 
 window.addEventListener("load", function() {
@@ -14,13 +18,75 @@ window.addEventListener("load", function() {
 
 });
 
-function changeStartView() {
-    // console.log("CheckBugs");
-    if(!start) {
-        document.getElementsByClassName('startView')[0].style.display = "none";
-        start = true;
-    } else {
-        document.getElementsByClassName('startView')[0].style.display = "block";
-        start = false;
+function chooseSide() {
+    if(singlePlayer) {
+        var singleChose = document.createElement("div");
+        singleChose.className = "singleChose";
+        var ask = document.createElement("h1");
+        ask.innerText = "You wanna X or O?";
+        singleChose.appendChild(ask);
+        var x = document.createElement("div");
+        x.className = "sideX";
+        x.innerText = "X";
+        singleChose.appendChild(x);
+        var o = document.createElement("div");
+        o.className = "sideO";
+        o.innerText = "O";
+        singleChose.appendChild(o);
+        backBoad[0].appendChild(singleChose);
+        x.addEventListener("click", playWithX);
+        o.addEventListener("click", playWithO);
+    } else if (twoPlayer) {
+        var twoChose = document.createElement("div");
+        twoChose.className = "twoChose";
+        var ask = document.createElement("h1");
+        ask.innerText = "You wanna X or O to move first?";
+        twoChose.appendChild(ask);
+        var x = document.createElement("div");
+        x.className = "sideX";
+        x.innerText = "X";
+        twoChose.appendChild(x);
+        var o = document.createElement("div");
+        o.className = "sideO";
+        o.innerText = "O";
+        twoChose.appendChild(o);
+        backBoad[0].appendChild(twoChose);
+        x.addEventListener("click", playWithX);
+        o.addEventListener("click", playWithO);
     }
+}
+
+function playWithX() {
+    showGrids();
+    turn = "X";
+    if(singlePlayer) {
+
+    } else if(twoPlayer) {
+        for(var i = 0; i < 3; i++) {
+            for(var j = 0; j < 3; j++) {
+                var box = document.getElementsByClassName("cell-" + i + "-" + j);
+                if(turn === "X") {
+                    box[0].addEventListener("click", setTimeOut(drawX(box), 500));
+                    turn = "O";
+                } else if(turn === "O") {
+                    box[0].addEventListener("click", setTimeOut(drawO(box), 500));
+                    turn = "X";
+                }
+            }
+        }
+    }
+}
+
+function playWithO() {
+    turn = "O";
+    showGrids();
+    if(singlePlayer) {
+
+    } else if(twoPlayer) {
+
+    }
+}
+
+function drawX(box) {
+    box.innerText = "X";
 }
--- pj2-build_a_tic_tac_toe_game/scripts/view.js
+++ pj2-build_a_tic_tac_toe_game/scripts/view.js
@@ -1,11 +1,51 @@
+function changeStartView() {
+    // console.log("CheckBugs");
+    if(!start) {
+        document.getElementsByClassName('startView')[0].style.display = "none";
+        start = true;
+    } else {
+        document.getElementsByClassName('startView')[0].style.display = "block";
+        start = false;
+    }
+}
+
 function singlePlayerView() {
     changeStartView();
     singlePlayer = true;
-    showGrids();
+    chooseSide();
+
 }
 
 function twoPlayerView() {
     changeStartView();
     twoPlayer = true;
-    showGrids();
+    chooseSide();
+}
+
+function showGrids() {
+    if(singlePlayer) {
+        document.getElementsByClassName("singleChose")[0].style.display = "none";
+    } else if(twoPlayer) {
+        document.getElementsByClassName("twoChose")[0].style.display = "none";
+    }
+    var drawBoard = document.createElement("div");
+    drawBoard.className = "drawBoard";
+    for(var i = 0; i < 3; i++) {
+        for(var j = 0; j < 3; j++) {
+            var box = document.createElement("div");
+            box.className = "cell-" + i + "-" + j;
+            drawBoard.appendChild(box);
+            updateGrids(box, i, j);
+        }
+    }
+    backBoad[0].appendChild(drawBoard);
+}
+
+function updateGrids(box, row, column) {
+    box.style.width = "100px";
+    box.style.height = "100px";
+    box.style.position = "absolute";
+    box.style.border = "1px solid white";
+    box.style.top = row * 100 + "px";
+    box.style.left = column * 100 + "px";
 }
